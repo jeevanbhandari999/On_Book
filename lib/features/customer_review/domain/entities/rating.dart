786cc023633dd86212904b5b3ed4911c8de9e171
Jeevan <bhandarigone320@gmail.com>
2026-01-15 10:41:34 -0800

    feat:customer_review, implemented rating entity

---
+++ lib/features/customer_review/domain/entities/rating.dart
@@ -0,0 +1,52 @@
+import 'package:equatable/equatable.dart';
+
+class Rating extends Equatable {
+  final String id;
+  final String postId;
+  final String userId;
+  final int ratingValue;
+  final String comment;
+  final DateTime createdAt;
+  final DateTime updatedAt;
+
+  const Rating({
+    required this.id,
+    required this.postId,
+    required this.userId,
+    required this.ratingValue,
+    required this.comment,
+    required this.createdAt,
+    required this.updatedAt,
+  });
+
+  Rating copyWith({
+    String? id,
+    String? postId,
+    String? userId,
+    int? ratingValue,
+    String? comment,
+    DateTime? createdAt,
+    DateTime? updatedAt,
+  }) {
+    return Rating(
+      id: id ?? this.id,
+      postId: postId ?? this.postId,
+      userId: userId ?? this.userId,
+      ratingValue: ratingValue ?? this.ratingValue,
+      comment: comment ?? this.comment,
+      createdAt: createdAt ?? this.createdAt,
+      updatedAt: updatedAt ?? this.updatedAt,
+    );
+  }
+
+  @override
+  List<Object> get props => [
+    id,
+    postId,
+    userId,
+    ratingValue,
+    comment,
+    createdAt,
+    updatedAt,
+  ];
+}
