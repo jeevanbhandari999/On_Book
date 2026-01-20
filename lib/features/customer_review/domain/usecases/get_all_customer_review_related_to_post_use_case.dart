3ea192a8872a10cb43cfbf28e604e250cf8fcc9c
Jeevan <bhandarijeevan320@gmail.com>
2026-01-20 10:55:48 -0800

    feat:custoemr_review, implemented get all customer review related to the post

---
+++ lib/features/customer_review/domain/usecases/get_all_customer_review_related_to_post_use_case.dart
@@ -0,0 +1,39 @@
+import 'package:app/core/errors/failures.dart';
+import 'package:app/features/customer_review/domain/entities/rating.dart';
+import 'package:app/features/customer_review/domain/repositories/customer_review_repository.dart';
+import 'package:dartz/dartz.dart';
+import 'package:equatable/equatable.dart';
+
+class GetAllCustomerReviewRelatedToPostUseCase {
+  final CustomerReviewRepository repository;
+
+  GetAllCustomerReviewRelatedToPostUseCase(this.repository);
+
+  Future<Either<Failure, List<Rating>>> call(
+    GetAllCustomerReviewRelatedToPostParams params,
+  ) async {
+    // Validate post ID
+    if (params.postId.trim().isEmpty) {
+      return const Left(ValidationFailure('Post ID is required'));
+    }
+
+    final data = await repository.getAllUserRatingsRelatedToThePost(
+      params.postId,
+    );
+
+    return data;
+  }
+}
+
+class GetAllCustomerReviewRelatedToPostParams extends Equatable {
+  final String postId;
+  final String? userId; // Made optional for
+
+  const GetAllCustomerReviewRelatedToPostParams({
+    required this.postId,
+    required this.userId,
+  });
+
+  @override
+  List<Object?> get props => [postId, userId];
+}
