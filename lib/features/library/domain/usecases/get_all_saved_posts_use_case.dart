d197a440941f0e723cfdaa8f3aa111ca413cac96
Jeevan <bhandarijeevan320@gmail.com>
2026-02-07 03:07:04 -0800

    feat:library, implemented the usecase for the get all saved posts related to the user:

--- lib/features/library/domain/usecases/get_all_saved_posts_use_case.dart
+++ lib/features/library/domain/usecases/get_all_saved_posts_use_case.dart
@@ -1,23 +1,30 @@
-// import 'package:app/core/errors/failures.dart';
-// import 'package:app/features/library/domain/repositories/library_repository.dart';
-// import 'package:app/features/post/domain/entities/post.dart';
-// import 'package:dartz/dartz.dart';
-// import 'package:equatable/equatable.dart';
+import 'package:app/core/errors/failures.dart';
+import 'package:app/features/library/domain/repositories/library_repository.dart';
+import 'package:app/features/post/domain/entities/post.dart';
+import 'package:dartz/dartz.dart';
+import 'package:equatable/equatable.dart';
 
-// class GetAllSavedPostsUseCase {
-//   final LibraryRepository repository;
-//   GetAllSavedPostsUseCase(this.repository);
+class GetAllSavedPostsUseCase {
+  final LibraryRepository repository;
+  GetAllSavedPostsUseCase(this.repository);
 
-//   Future<Either< Failure, List<Post>>> call() async {
+  Future<Either<Failure, List<Post>>> call(
+    GetAllSavedPostsParams params,
+  ) async {
+    // First of all validate the required fields;
+    if (params.userId.trim().isEmpty) {
+      return const Left(ValidationFailure('User id is required'));
+    }
 
-//   }
-// }
+    return await repository.getAllSavedPosts(params.userId);
+  }
+}
 
-// class GetAllSavedPostsParams extends Equatable {
-//   final String userId;
+class GetAllSavedPostsParams extends Equatable {
+  final String userId;
 
-//   const GetAllSavedPostsParams({required this.userId});
+  const GetAllSavedPostsParams({required this.userId});
 
-//   @override
-//   List<Object> get props => [userId];
-// }
+  @override
+  List<Object> get props => [userId];
+}
