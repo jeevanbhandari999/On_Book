5fa685952895f495d54050ead77e49bd2de460d5
Jeevan <bhandarijeevan320@gmail.com>
2026-02-14 04:15:47 -0800

    feat:chat, implemented the get specific room related to the user and the organization use case

---
+++ lib/features/chat/domain/usecases/get_specific_room_related_to_the_user_or_organization_use_case.dart
@@ -0,0 +1,41 @@
+import 'package:app/core/errors/failures.dart';
+import 'package:app/features/chat/domain/entities/room.dart';
+import 'package:app/features/chat/domain/repositories/chat_repository.dart';
+import 'package:dartz/dartz.dart';
+import 'package:equatable/equatable.dart';
+
+class GetSpecificRoomRelatedToTheUserOrOrganizationUseCase {
+  final ChatRepository repository;
+
+  GetSpecificRoomRelatedToTheUserOrOrganizationUseCase(this.repository);
+
+  Future<Either<Failure, Room?>> call(
+    GetSpecificRoomRelatedToTheUserOrOrganizationParams params,
+  ) async {
+    // First validate the required fields
+    if (params.userId.trim().isEmpty) {
+      return const Left(ValidationFailure('User id is required'));
+    }
+
+    return await repository.getSpecificRoom(
+      params.userId,
+      params.targetUserId,
+      params.organizationId,
+    );
+  }
+}
+
+class GetSpecificRoomRelatedToTheUserOrOrganizationParams extends Equatable {
+  final String userId;
+  final String? targetUserId;
+  final String? organizationId;
+
+  const GetSpecificRoomRelatedToTheUserOrOrganizationParams({
+    required this.userId,
+    this.targetUserId,
+    this.organizationId,
+  });
+
+  @override
+  List<Object?> get props => [userId, targetUserId, organizationId];
+}
