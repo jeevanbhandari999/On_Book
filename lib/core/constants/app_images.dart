15b2cfacd85672ddb57b53a39e8cb95501c2bb88
Jeevan <bhandarijeevan320@gmail.com>
2026-03-05 23:32:14 -0800

    app images for unified all images path

---
+++ lib/core/constants/app_images.dart
@@ -0,0 +1,32 @@
+class AppImages {
+  // Right now let's use the dummy image
+
+  // JPEG, WEBP, PNG(assets)
+  // Logo images
+
+  // svg files(assets)
+
+  // Icons(svg files)
+  static const String eyeCloseIcon = 'assets/svgs/eye_close_icon.svg';
+  static const String eyeOpenIcon = 'assets/svgs/eye_open_icon.svg';
+  static const String googleIcon = 'assets/svgs/google_icon.svg';
+  static const String esewaIcon = 'assets/svgs/esewa_icon.svg';
+  static const String khaltiIcon = 'assets/svgs/khalti_icon.svg';
+  static const String connectIpsIcon = 'assets/svgs/connect_ips_icon.svg';
+  static const String connectIPSIcon = 'assets/svgs/connect_icon.svg';
+  static const String arrowDown = 'assets/svgs/arrow_down.svg';
+  static const String imePayIcon = 'assets/svgs/ime_pay_icon.svg';
+  static const String bankTransferIcon = 'assets/svgs/bank_transfer_icon.svg';
+  static const String menuIcon = 'assets/svgs/menu_icon.svg';
+  static const String homeNavBarIcon = 'assets/svgs/home_nav_bar.svg';
+  static const String chatNavBarIcon = 'assets/svgs/chat_nav_bar.svg';
+  static const String documentNavBarIcon = 'assets/svgs/document_nav_bar.svg';
+  static const String notificationNavBarIcon =
+      'assets/svgs/notification_nav_bar.svg';
+  static const String onBoardingCheckIcon =
+      'assets/svgs/on_boarding_check_icon.svg';
+  static const String manWithBooks = 'assets/svgs/man_with_books.svg';
+  static const String organizationIcon = 'assets/svgs/organization.svg';
+
+  // Network images
+}
