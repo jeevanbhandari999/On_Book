c13bb4c5353908e549455cbb62bd0d5731cf6188
Jeevan <bhandarijeevan320@gmail.com>
2026-03-06 20:53:53 -0800

    refactored the Ui Constats to handle consistency in animations also

--- lib/core/constants/ui_constants.dart
+++ lib/core/constants/ui_constants.dart
@@ -13,6 +13,7 @@ class UiConstants {
   static const double radiusMd = 12.0;
   static const double radiusLg = 16.0;
   static const double radiusXl = 24.0;
+  static const double radiusXxl = 32.0;
   static const double radiusRound = 999.0;
 
   // Icon Sizes
@@ -20,7 +21,25 @@ class UiConstants {
   static const double iconSm = 20.0;
   static const double iconMd = 24.0;
   static const double iconLg = 32.0;
-  static const double iconXl = 48.0;
+  static const double iconXl = 40.0;
+  static const double iconXxl = 50.0;
+  static const double icon2Xl = 60.0;
+
+  // Text Sized
+  static const double textXxs = 8.0;
+  static const double textXs = 10.0;
+  static const double textSm = 12.0;
+  static const double textMd = 14.0;
+  static const double textLg = 16.0;
+  static const double textXl = 18.0;
+  static const double text2Xl = 20.0;
+  static const double text3Xl = 22.0;
+  static const double text4Xl = 24.0;
+  static const double text5Xl = 26.0;
+  static const double text6Xl = 28.0;
+  static const double text7Xl = 30.0;
+  static const double text8Xl = 32.0;
+  static const double text9Xl = 34.0;
 
   // Button Heights
   static const double buttonHeightSm = 36.0;
@@ -31,6 +50,21 @@ class UiConstants {
   static const Duration animationFast = Duration(milliseconds: 150);
   static const Duration animationNormal = Duration(milliseconds: 300);
   static const Duration animationSlow = Duration(milliseconds: 500);
+  static const Duration animationSlower = Duration(milliseconds: 700);
+  static const Duration animationSlowest = Duration(milliseconds: 900);
+
+  static const Duration animationDelayFastest = Duration(milliseconds: 100);
+  static const Duration animationDelayFaster = Duration(milliseconds: 200);
+  static const Duration animationDelayFast = Duration(milliseconds: 300);
+  static const Duration animationDelayNormal = Duration(milliseconds: 400);
+  static const Duration animationDelaySlow = Duration(milliseconds: 500);
+  static const Duration animationDelaySlower = Duration(milliseconds: 100);
+  static const Duration animationDelaySlowest = Duration(milliseconds: 100);
+  static const Duration animationDelayFirst = Duration(milliseconds: 600);
+  static const Duration animationDelaySecond = Duration(milliseconds: 700);
+  static const Duration animationDelayThird = Duration(milliseconds: 800);
+  static const Duration animationDelayFourth = Duration(milliseconds: 900);
+  static const Duration animationDelayFifth = Duration(milliseconds: 1000);
 
   // Elevation
   static const double elevationNone = 0.0;
